9088e39806e00f417ef73b8fbd0f7b9b45e6152e
Md-ismail47 <80391110+Md-ismail47@users.noreply.github.com>
2022-12-07 15:35:04 +0530

    variable added

--- src/com/Blackthron/web/allScripts/NewTestingClass.java
+++ src/com/Blackthron/web/allScripts/NewTestingClass.java
@@ -2,7 +2,9 @@ package com.Blackthron.web.allScripts;
 
 public class NewTestingClass {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{ 
+		int a=10;
 		// TODO Auto-generated method stub
 
 	}
